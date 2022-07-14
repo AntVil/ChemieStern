3712dd82c19db6de4589d6966245ae68b9938cc9
AntVil <56440776+AntVil@users.noreply.github.com>
2022-07-14 16:34:25 +0200

    update

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -20,13 +20,17 @@ const CONTENT = [
 ];
 
 
+const CONTENT_TYPE = Symbol("text_type");
+
+
 /* page contents */
 let contents;
 
 
 async function loadContent(contents, contentName){
     try{
-        contents[contentName] = (await (await fetch(`./content/${contentName}/content.txt`)).text()).toString().replaceAll("\r", "");
+        let content = (await (await fetch(`./content/${contentName}/content.txt`)).text()).toString();
+        contents[contentName] = content.replaceAll("\r", "").replaceAll(/\n\n+/g, "\n\n");
     }catch{
         console.warn(`could not find ${contentName}`);
     }
@@ -43,7 +47,7 @@ function getContentName(contentName){
     return contents[contentName].split("---")[1].split("\n")[1].trim()
 }
 
-function getContentCard(contentName){
+function getContentCardList(contentName){
     let result = contents[contentName].split("---")[1].split("\n").slice(2).map((a) => a.trim()).filter((a) => a.length > 0);
     return result;
 }
@@ -66,153 +70,130 @@ function getContentText(contentName, removeSpecial){
     return result
 }
 
-function renderContent(contentName){
-    let card = getContentCard(contentName);
-    
-    let cardElement = document.getElementById("contentCard");
-    cardElement.innerHTML = "";
-    for(let row of card){
-        let items = row.split(":");
-        let rowElement = document.createElement("div");
-        rowElement.style.display = "grid";
-        rowElement.style.gridTemplateColumns = `repeat(${items.length}, 1fr)`;
-        for(let item of items){
-            item = item.trim();
-            let itemElement;
-            if(item.startsWith("[") && item.endsWith("]")){
-                itemElement = document.createElement("img");
-                itemElement.src = `./content/${contentName}/images/${item.slice(1, item.length-1)}`;
-            }else if(item.includes("$")){
-                if(item.startsWith("$$") && item.endsWith("$$")){
-                    itemElement = document.createElement("div");
-                    katex.render(item.slice(2, item.length-2), itemElement, {
-                        throwOnError: false
-                    });
-                    itemElement.style.textAlign = "center";
-                }else{
-                    itemElement = document.createElement("div");
-                    for(let sub of item.match(/\$([^\$]*)\$/g)){
-                        item = item.replace(sub, katex.renderToString(sub.slice(1, sub.length-1), {
-                            throwOnError: false
-                        }));
-                    }
-                    itemElement.innerHTML = item;
-                }
-            }else{
-                itemElement = document.createElement("div");
-                itemElement.innerText = item;
-            }
-            
-            rowElement.appendChild(itemElement);
-        }
-        cardElement.appendChild(rowElement);
-        cardElement.classList.add("table");
-    }
+function renderContentImage(contentName, imageSrc){
+    let element = document.createElement("img");
+    element[CONTENT_TYPE] = "img";
+    element.src = `./content/${contentName}/images/${imageSrc}`;
+    return element;
+}
 
+function renderContentInlineMath(contentName, text){
+    let element = document.createElement("div");
+    element[CONTENT_TYPE] = "inlinemath";
+    for(let sub of text.match(/\$([^\$]*)\$/g)){
+        text = text.replace(sub, katex.renderToString(sub.slice(1, sub.length-1), {
+            throwOnError: false
+        }));
+    }
+    element.innerHTML = text;
+    return element;
+}
+
+function renderContentBlockMath(contentName, math){
+    let element = document.createElement("div");
+    element[CONTENT_TYPE] = "blockmath";
+    katex.render(math, element, {
+        throwOnError: false
+    });
+    return element;
+}
+
+function renderContentHeading(contentName, text){
+    let element = document.createElement("div");
+    element[CONTENT_TYPE] = "heading";
+    element.innerText = text;
+    element.style.fontWeight = "bold";
+    element.style.textDecoration = "underline";
+    return element;
+}
+
+function renderContentTextline(contentName, text){
+    let element = document.createElement("div");
+    element[CONTENT_TYPE] = "text";
+    element.innerText = text;
+    return element;
+}
+
+
+function renderContentItem(contentName, item){
+    item = item.trim();
+    if(item.startsWith("[") && item.endsWith("]")){
+        return renderContentImage(contentName, item.slice(1, item.length-1));
+    }else if(item.includes("$")){
+        if(item.startsWith("$$") && item.endsWith("$$")){
+            return renderContentBlockMath(contentName, item.slice(2, item.length-2));
+        }else{
+            return renderContentInlineMath(contentName, item);
+        }
+    }else if(item.startsWith("#")){
+        return renderContentHeading(contentName, item.slice(1).trim());
+    }else{
+        return renderContentTextline(contentName, item);
+    }
+}
+
+
+function renderContentTableRow(contentName, row){
+    let rowElement = document.createElement("div");
+    let items = row.split(":");
+    for(let item of items){
+        let itemElement = renderContentItem(contentName, item);
+        rowElement.appendChild(itemElement);
+    }
+    rowElement.style.display = "grid";
+    rowElement.style.gridTemplateColumns = `repeat(${items.length}, 1fr)`;
+    return rowElement;
+}
+
+
+function renderContent(contentName){
+    // title
     document.getElementById("contentTitle").innerText = getContentName(contentName);
 
+    // card
+    let cardList = getContentCardList(contentName);
+    let cardElement = document.getElementById("contentCard");
+    cardElement.innerHTML = "";
+    for(let row of cardList){
+        let rowElement = renderContentTableRow(contentName, row);
+        cardElement.appendChild(rowElement)
+    }
+    cardElement.classList.add("table");
+    
+    // main
     let text = getContentText(contentName, false);
     let textElement = document.getElementById("contentText");
     textElement.innerHTML = "";
-    let insideTable = false;
-    let previousElement = "none";
-    let tableContent = [];
 
-    for(let line of text.replaceAll(/\n\n+/g, "\n\n").split("\n")){
+    let isTable = false;
+    let tableElement = document.createElement("div");
+    
+    for(let line of text.split("\n")){
         line = line.trim();
-
-        let lineElement;
-
-        if(line.length == 0){
-            lineElement = document.createElement("br");
-            previousElement = "linebreak";
-        }else if(line.startsWith("{") || insideTable){
-            insideTable = true;
-            tableContent.push(line);
+        if(isTable || line.startsWith("{")){
             if(line.endsWith("}")){
-                insideTable = false;
-                lineElement = document.createElement("div")
-                for(let row of tableContent){
-                    let items = row.replaceAll(/\{|\}/g, "").split(":");
-                    let rowElement = document.createElement("div");
-                    rowElement.style.display = "grid";
-                    rowElement.style.gridTemplateColumns = `repeat(${items.length}, 1fr)`;
-                    for(let item of items){
-                        item = item.trim()
-                        let itemElement;
-                        if(item.startsWith("[") && item.endsWith("]")){
-                            itemElement = document.createElement("img");
-                            itemElement.src = `./content/${contentName}/images/${item.slice(1, item.length-1)}`;
-                        }else if(item.includes("$")){
-                            if(item.startsWith("$$") && item.endsWith("$$")){
-                                itemElement = document.createElement("div");
-                                katex.render(item.slice(2, item.length-2), itemElement, {
-                                    throwOnError: false
-                                });
-                                itemElement.style.textAlign = "center";
-                            }else{
-                                itemElement = document.createElement("div");
-                                for(let sub of item.match(/\$([^\$]*)\$/g)){
-                                    console.log(sub);
-                                    item = item.replace(sub, katex.renderToString(sub.slice(1, sub.length-1), {
-                                        throwOnError: false
-                                    }));
-                                }
-                                itemElement.innerHTML = item;
-                            }
-                        }else{
-                            itemElement = document.createElement("div");
-                            itemElement.innerText = item;
-                        }
-                        
-                        rowElement.appendChild(itemElement);
-                    }
-                    if(rowElement.innerText !== ""){
-                        lineElement.appendChild(rowElement);
-                    }
+                isTable = false;
+                tableElement.classList.add("table");
+                textElement.appendChild(tableElement);
+                tableElement = document.createElement("div")
+            }else{
+                isTable = true;
+                if(line.startsWith("{")){
+                    continue;
                 }
-                lineElement.classList.add("table")
+                tableElement.appendChild(renderContentTableRow(contentName, line));
             }
-            previousElement = "table";
-        }else if(line.startsWith("#")){
-            lineElement = document.createElement("div");
-            lineElement.innerText = line.replaceAll(/\#*/g, "");
-            lineElement.style.fontWeight = "bold";
-            lineElement.style.textDecoration = "underline";
-            previousElement = "heading";
-        }else if(line.startsWith("[") && line.endsWith("]")){
-            lineElement = document.createElement("img");
-            lineElement.src = `./content/${contentName}/images/${line.slice(1, line.length-1)}`;
-            previousElement = "image";
-        }else if(line.startsWith("$$") && line.endsWith("$$")){
-            lineElement = document.createElement("div");
-            katex.render(line.slice(2, line.length-2), lineElement, {
-                throwOnError: false
-            });
-            lineElement.style.textAlign = "center";
-            previousElement = "math";
-        }else if(line.includes("$")){
-            lineElement = document.createElement("div");
-            for(let sub of line.match(/\$([^\$]*)\$/g)){
-                line = line.replace(sub, katex.renderToString(sub.slice(1, sub.length-1), {
-                    throwOnError: false
-                }));
-            }
-            lineElement.innerHTML = line;
-            previousElement = "math";
+        }else if(line.length === 0){
+            textElement.appendChild(document.createElement("br"));
         }else{
-            if(previousElement === "text"){
-                let element = textElement.children[textElement.children.length - 1];
-                element.innerText = `${element.innerText} ${line}`.replaceAll(/  +/g, " ");
-                continue;
+            let element = renderContentItem(contentName, line);
+            let previousElement = textElement.lastElementChild;
+
+            if(previousElement !== null && (element[CONTENT_TYPE] === "text" && previousElement[CONTENT_TYPE] === "text")){
+                previousElement.innerText += ` ${element.innerText}`;
+            }else{
+                textElement.appendChild(element);
             }
-            lineElement = document.createElement("div");
-            lineElement.innerText = line;
-            previousElement = "text";
-        }
-        
-        if(!insideTable && lineElement !== undefined){
-            textElement.appendChild(lineElement);
         }
     }
 }
