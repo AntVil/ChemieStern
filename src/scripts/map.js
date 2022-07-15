e6bd7a1ae21ad72cb415ab4026f01c73e149b487
AntVil <antboehler@gmail.com>
2022-07-16 01:53:27 +0200

    update

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,7 +1,14 @@
 const MAP_SCALING_FACTOR = 1.5;
-const SIMULATION_STEPS = 1;
-const GRAPH_OFFSET_X = 50;
-const GRAPH_OFFSET_Y = 50;
+const SIMULATION_STEPS = 10;
+const SIMULATION_REPULSION_STRENGTH = 0.1;
+const SIMULATION_FRICTION_STRENGTH = 0.3;
+const SIMULATION_ATTRACTION_STRENGTH = 0.4;
+const GRAPH_OFFSET_X = 250;
+const GRAPH_OFFSET_Y = 100;
+const NODE_WIDTH = 230;
+const NODE_HEIGHT = 50;
+const NODE_RADIUS = 15;
+const NODE_FONT_SIZE = 25;
 
 let mapCanvas;
 let ctxt;
@@ -54,8 +61,6 @@ async function mapSetup(){
         e.preventDefault();
         endPointer();
     });
-    
-    pointerDragging = false;
 
     mapTransform = [
         1, 0, 0,
@@ -63,7 +68,9 @@ async function mapSetup(){
         0, 0, 1
     ];
     pointerOffsetPosition = [0, 0];
+    pointerDragging = false;
 
+    // setup graph
     graph = [];
     for(let content of Object.keys(contents)){
         graph[content] = {
@@ -73,6 +80,7 @@ async function mapSetup(){
         };
     }
 
+    // get root node
     let rootNodes = new Set(Object.keys(graph));
     while(rootNodes.size > 1){
         let temp = new Set();
@@ -83,6 +91,7 @@ async function mapSetup(){
         rootNodes = temp;
     }
     
+    // put nodes into layers
     let layers = [Array.from(rootNodes)];
     let addedNodes = new Set(rootNodes);
     while(addedNodes.size !== Object.keys(graph).length){
@@ -96,21 +105,16 @@ async function mapSetup(){
         layers.push(layer);
         layer.forEach((n) => addedNodes.add(n));
     }
-
-
-    
     for(let i=0;i<layers.length;i++){
         for(let j=0;j<layers[i].length;j++){
             graph[layers[i][j]].x = j;
             graph[layers[i][j]].y = i;
         }
     }
-
-    console.log(layers)
-
+    
+    // insert extra nodes on layers between child and parent
     let extraNodeId = 0;
     for(let i=layers.length-1;i>=0;i--){
-        console.log(i, layers)
         for(let j=0;j<layers[i].length;j++){
             let node = graph[layers[i][j]];
             let parents = node.parents;
@@ -121,9 +125,9 @@ async function mapSetup(){
                 }
 
                 let extraNode;
+                let firstExtraNodeName = `_${extraNodeId}`;
                 for(let l=0;l<distance-1;l++){
                     let y = i - l - 1;
-                    console.log("y", y)
                     let extraNodeName = `_${extraNodeId}`;
                     extraNode = {
                         "x": layers[y].length,
@@ -135,19 +139,45 @@ async function mapSetup(){
                     extraNodeId++;
                 }
                 extraNode.parents = [parents[k]];
+                parents[k] = firstExtraNodeName;
             }
         }
     }
 
+    // make simulation deterministic
+    for(let i=0;i<layers.length;i++){
+        layers[i] = layers[i].sort();
+        for(let j=0;j<layers[i].length;j++){
+            graph[layers[i][j]].x = j;
+            graph[layers[i][j]].dx = 0;
+        }
+    }    
+
+    // simulate pushing and pulling
     for(let t=0;t<SIMULATION_STEPS;t++){
+        for(let i=0;i<layers.length-1;i++){
+            for(let j=0;j<layers[i].length;j++){
+                let parents = graph[layers[i][j]].parents
+                for(let k=0;k<parents.length;k++){
+                    graph[layers[i][j]].dx += SIMULATION_ATTRACTION_STRENGTH * (graph[parents[k]].x - graph[layers[i][j]].x);
+                    graph[parents[k]].dx += SIMULATION_ATTRACTION_STRENGTH * (graph[layers[i][j]].x - graph[parents[k]].x);
+                }
+                for(let k=0;k<layers[i].length;k++){
+                    graph[layers[i][j]].dx -= SIMULATION_REPULSION_STRENGTH * Math.cbrt(graph[layers[i][k]].x - graph[layers[i][j]].x);
+                }
+            }
+        }
         for(let i=0;i<layers.length;i++){
             for(let j=0;j<layers[i].length;j++){
-
+                graph[layers[i][j]].dx *= SIMULATION_FRICTION_STRENGTH;
+                graph[layers[i][j]].x += graph[layers[i][j]].dx;
             }
         }
     }
-    
+
+    // set final positions
     for(let i=0;i<layers.length;i++){
+        layers[i] = layers[i].sort((a, b) => graph[a].x - graph[b].x)
         for(let j=0;j<layers[i].length;j++){
             graph[layers[i][j]].x = (j - 0.5 * (layers[i].length-1)) * GRAPH_OFFSET_X;
             graph[layers[i][j]].y = -i * GRAPH_OFFSET_Y;
@@ -209,21 +239,45 @@ function mapRender(){
     ctxt.setTransform(mapTransform[0], mapTransform[3], mapTransform[1], mapTransform[4], mapTransform[2] + offset[0], mapTransform[5] + offset[1]);
 
     for(let node of Object.keys(graph)){
-        ctxt.fillRect(graph[node].x-10, graph[node].y-10, 20, 20);
-
         for(let parent of graph[node].parents){
             ctxt.beginPath();
             ctxt.moveTo(graph[node].x, graph[node].y);
-            ctxt.lineTo(graph[parent].x, graph[parent].y);
+            ctxt.bezierCurveTo(graph[node].x, graph[parent].y, graph[parent].x, graph[node].y, graph[parent].x, graph[parent].y);
             ctxt.stroke();
         }
     }
-    
-    
+
+    for(let node of Object.keys(graph)){
+        if(!node.startsWith("_")){
+            ctxt.fillStyle = "#AAAAAA";
+            roundedRect(ctxt, graph[node].x - NODE_WIDTH / 2, graph[node].y - NODE_HEIGHT / 2, NODE_WIDTH, NODE_HEIGHT, NODE_RADIUS);
+            ctxt.fill();
+            ctxt.textAlign = "center";
+            ctxt.textBaseline = "middle";
+            ctxt.fillStyle = "#333333";
+            ctxt.font = `${NODE_FONT_SIZE}px Arial`;
+            ctxt.fillText(node, graph[node].x, graph[node].y)
+        }
+    }
 
     ctxt.restore();
 }
 
+function roundedRect(ctxt, x, y, width, height, radius) {
+    ctxt.beginPath();
+    ctxt.moveTo(x + radius, y);
+    ctxt.lineTo(x + width - radius, y);
+    ctxt.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctxt.lineTo(x + width, y + height - radius);
+    ctxt.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctxt.lineTo(x + radius, y + height);
+    ctxt.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctxt.lineTo(x, y + radius);
+    ctxt.quadraticCurveTo(x, y, x + radius, y);
+    ctxt.closePath();
+}
+
+/* transfrom calculations (for zoom) */
 function matrixMultiplication(matrix1, matrix2){
     let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     for(let i=0;i<3;i++){
@@ -235,7 +289,6 @@ function matrixMultiplication(matrix1, matrix2){
     }
     return result;
 }
-
 
 function matrixInverse(m){
     let det = m[0] * (m[4] * m[8] - m[7] * m[5]) - m[1] * (m[3] * m[8] - m[5] * m[6]) + m[2] * (m[3] * m[7] - m[4] * m[6]);
